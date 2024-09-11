d7ee9892814a0e65742b3c54378dd7d6e7d38758
Eliott <e.tan@student.isartdigital.com>
2024-09-11 09:20:49 +0200

    importing scene transition

---
+++ Assets/TransitionManager/Scripts/Transition.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.EliottTan.SceneTransitions
+{
+    /// <summary>
+    /// Component used to create transition between scenes or screens
+    /// </summary>
+    public class Transition : MonoBehaviour
+    {
+        //Animator and triggers
+        public Animator animator { get; private set; }
+        string startTrigger = "StartTrigger";
+        string endTrigger = "TriggerEnd";
+
+        float maxAnimationProgress = .9f;
+
+        bool startAnimationFinished = false;
+
+        public event Action<float> onAsyncLoadProgress;
+        public event Action onStartAnimationFinished;
+
+        enum TransitionType { toScene, toMethod}
+        TransitionType transitionType;
+
+        public async void TransitionToNextScene(int pSceneIndex,LoadSceneMode pMode = LoadSceneMode.Single)
+        {
+            transitionType = TransitionType.toScene;
+            StartAnimation();
+
+            while (!startAnimationFinished)
+            {
+                await Task.Delay(1000);
+            }
+
+            AsyncOperation lNextScene = SceneManager.LoadSceneAsync(pSceneIndex,pMode);
+
+            lNextScene.allowSceneActivation = false;
+            while (lNextScene.progress < maxAnimationProgress)
+            {
+                onAsyncLoadProgress?.Invoke(lNextScene.progress);
+                await Task.Delay(1000);
+            }
+
+            lNextScene.allowSceneActivation = true;
+            animator.SetTrigger(endTrigger);
+        }
+
+        public async void TransitionToNextScene(string pSceneName, LoadSceneMode pMode = LoadSceneMode.Single)
+        {
+            StartAnimation();
+
+            while (!startAnimationFinished)
+            {
+                await Task.Delay(1000);
+            }
+
+            AsyncOperation lNextScene = SceneManager.LoadSceneAsync(pSceneName, pMode);
+
+            lNextScene.allowSceneActivation = false;
+            while (lNextScene.progress < maxAnimationProgress)
+            {
+                onAsyncLoadProgress?.Invoke(lNextScene.progress);
+                await Task.Delay(1000);
+            }
+
+            lNextScene.allowSceneActivation = true;
+            animator.SetTrigger(endTrigger);
+        }
+
+        private void StartAnimation()
+        {
+            animator = GetComponent<Animator>();
+            DontDestroyOnLoad(gameObject);
+            startAnimationFinished = false;
+            animator.SetTrigger(startTrigger);
+        }
+
+        public void TransitionToMethod()
+        {
+            transitionType = TransitionType.toMethod;
+            StartAnimation();
+        }
+
+        public void OnStartAnimationFinished()
+        {
+            onStartAnimationFinished?.Invoke();
+            startAnimationFinished = true;
+            if (transitionType == TransitionType.toMethod)
+            {
+                animator.SetTrigger(endTrigger);
+            }
+        }
+
+        public void OnEndAnimationFinished()
+        {
+            onStartAnimationFinished = null;
+            Destroy(gameObject);
+        }
+
+    }
+}
